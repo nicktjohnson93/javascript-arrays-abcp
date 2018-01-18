a846290c34bedd8f114ef3ebd7a3d6ae31471193
Nick Johnson <nicktjohnson93@gmail.com>
2018-01-18 02:25:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray (array, element) {
-  const newArray = ["twix", ...chocolateBars]
-c(newArray)
+  return array.
 }
 console.log(addElementToBeginningOfArray([chocolateBars], "twix"))
 
